590f481e865d2b87c4219d22e53ed43d4f95c48c
praveen-ak <ak@waioz.com>
2021-06-23 18:44:37 +0530

    first commit vijay

---
+++ node/graphql/commonHelper.js
@@ -0,0 +1,124 @@
+const fs = require('fs')
+const util = require('util')
+var FCM = require('fcm-node');
+var nodemailer = require('nodemailer');
+var serverKey = 'AAAAuHbsbq4:APA91bFf9VkaLpuWm0vfMTfDIofpl8Lz4ySnkJfW8w0tvZss0CR5ozVQ97As7hVVL4k0nN-rANxTvRLBxRH3XuEXkHICQi8FKcV6fuqJpqcRsI0YF4XuFODcojoqKZaJ-uz9H_to-9U2';
+var fcm = new FCM(serverKey);
+const env = process.env;
+
+
+module.exports.home=0;
+module.exports.pending=1;
+module.exports.on_going=2;
+module.exports.completed=3;
+module.exports.chat =4;
+module.exports.booking_view =5;
+
+module.exports.no_image = () => {
+  return env.APP_URL + '/images/public/no_img.jpg';
+}
+module.exports.siteName = () => {
+  return env.APP_NAME;
+}
+module.exports.getBaseurl = () => {
+  return env.APP_URL;
+}
+module.exports.getPaymenturl = () => {
+  return env.PAYMENT_URL;
+}
+module.exports.siteUrl = () => {
+  return env.APP_URL;
+}
+module.exports.prepareUploadFolder = (path) => {
+  const pathExist = fs.existsSync(path)
+  if (!pathExist) {
+    fs.mkdirSync(path, {
+      recursive: true
+    })
+  }
+}
+
+module.exports.push_notifiy = async (message) => {
+  //console.log("push work");
+  // console.log(message);
+  return await fcm.send(message, function (err, response) {
+    if (err) {
+      // console.log(err);
+      //console.log("Something has gone wrong!");
+    } else {
+      console.log("Successfully sent with response: ", response);
+    }
+  });
+};
+
+module.exports.send_mail = async (email, otp) => {
+  // console.log('vis',email,otp);
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure:true,
+    auth: {
+      user: 'picvi.acc@gmail.com',
+      pass: 'yumcyizjbsevpscg'
+    },
+    tls: {
+      // do not fail on invalid certs
+      rejectUnauthorized: false
+      }
+  });
+  let mailOptions = {
+    from: 'waioztechnology@gmail.com',
+    to: email, // list of receivers
+    subject: "JIFFY OTP ✔", // Subject line
+    text: "OTP?", // plain text body
+    html: `<b>JIFFY OTP : ${otp} </b>` // html body
+    // subject:strtr(admin_email.subject, req.data), // Subject line
+    // html: mail_content // html body
+  };
+
+  await transporter.sendMail(mailOptions, (error, info) => {
+    console.log(error,info,mailOptions)
+    if (error) {
+      console.error(error);
+      return false;
+    }
+    else {
+      console.error(info);
+      return true;
+    }
+  });
+}
+module.exports.send_mail_1 = async (email, msg) => {
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 25,
+    service: 'gmail',
+    auth: {
+      user: 'picvi.acc@gmail.com',
+      pass: 'yumcyizjbsevpscg'
+    }
+  });
+  let mailOptions = {
+    from: 'waioztechnology@gmail.com',
+    to: email, // list of receivers
+    subject: "JIFFY ✔", // Subject line
+    text: "Admin change proof status?", // plain text body
+    html: `<b> ${msg} </b>` // html body
+    // subject:strtr(admin_email.subject, req.data), // Subject line
+    // html: mail_content // html body
+  };
+  await transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      //console.error(error);
+      return false;
+    }
+    else {
+      // console.error(info);
+      return true;
+    }
+  });
+}
+
+/*
+  Tab No for Push Notification
+*/
