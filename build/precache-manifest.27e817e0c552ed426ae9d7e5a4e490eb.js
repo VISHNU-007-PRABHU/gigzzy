47f54faa5536f881a7bee0d713833480774f0e16
praveen-ak <ak@waioz.com>
2021-06-25 20:28:52 +0530

    changed serve

---
+++ build/precache-manifest.27e817e0c552ed426ae9d7e5a4e490eb.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c9138b90962d1a2296f5c6e327164ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b627ed46508c0b49764",
+    "url": "/static/css/7.ad9926b6.chunk.css"
+  },
+  {
+    "revision": "c43e832fa8205949dc32",
+    "url": "/static/css/main.0a051c1a.chunk.css"
+  },
+  {
+    "revision": "646946285d5b8f38d8ec",
+    "url": "/static/js/0.e4ef05ba.chunk.js"
+  },
+  {
+    "revision": "367da0ec782ec3c6438f",
+    "url": "/static/js/1.a2891831.chunk.js"
+  },
+  {
+    "revision": "e06b2cd58e8b21225cf2",
+    "url": "/static/js/10.fff4a91d.chunk.js"
+  },
+  {
+    "revision": "1fa89cd31b5fa6ac0178",
+    "url": "/static/js/11.62c9ce31.chunk.js"
+  },
+  {
+    "revision": "644183130117403eb672",
+    "url": "/static/js/2.4844f742.chunk.js"
+  },
+  {
+    "revision": "0fb0e98627f1379848cc",
+    "url": "/static/js/3.742b18fe.chunk.js"
+  },
+  {
+    "revision": "f10f2c19a0c1630ef548",
+    "url": "/static/js/4.c1f1a214.chunk.js"
+  },
+  {
+    "revision": "9b627ed46508c0b49764",
+    "url": "/static/js/7.0c0128fd.chunk.js"
+  },
+  {
+    "revision": "f8deec9dd62b0ca5a330",
+    "url": "/static/js/8.adb95a95.chunk.js"
+  },
+  {
+    "revision": "188bfca58e9a10efc855",
+    "url": "/static/js/9.fa67e19b.chunk.js"
+  },
+  {
+    "revision": "c43e832fa8205949dc32",
+    "url": "/static/js/main.ed6d6319.chunk.js"
+  },
+  {
+    "revision": "a1b449966adc23647c3a",
+    "url": "/static/js/runtime~main.3abc2cdc.js"
+  },
+  {
+    "revision": "ec60c0874ff357fd6bf28da6c348b78b",
+    "url": "/static/media/Gigzzy.ec60c087.png"
+  },
+  {
+    "revision": "3e2074676586b45449dcf05cdca01ca6",
+    "url": "/static/media/bookLater.3e207467.png"
+  },
+  {
+    "revision": "f24c9cae369f84c1553d9ec29c7cc9dc",
+    "url": "/static/media/gigzzypro.f24c9cae.png"
+  },
+  {
+    "revision": "05865d4f0f161de52f31c55efc757cdd",
+    "url": "/static/media/handyman.05865d4f.jpg"
+  },
+  {
+    "revision": "3f7f40b64b5ca89f288c22f8e10b1164",
+    "url": "/static/media/handyman2.3f7f40b6.jpg"
+  },
+  {
+    "revision": "906a6fbbb7e3d96fa103c01c2f476611",
+    "url": "/static/media/handyman3.906a6fbb.jpg"
+  },
+  {
+    "revision": "c57228a882cbf3bfe440b15d20efd7ce",
+    "url": "/static/media/main.c57228a8.png"
+  },
+  {
+    "revision": "cf173470f3d4490e5936219f2d1d589d",
+    "url": "/static/media/no_booking.cf173470.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
