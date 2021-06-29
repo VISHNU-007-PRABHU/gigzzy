037d681295d119383e09be84244d3fc7887f7bbd
praveen-ak <ak@waioz.com>
2021-06-29 13:24:02 +0530

    changes updatecatgory

--- node/graphql/resolvers/category.js
+++ node/graphql/resolvers/category.js
@@ -218,7 +218,7 @@ module.exports.updateCategory = async (parent, args, { file }) => {
     var fs = require('fs');
     if (args.category_name) {
         var pre_name = await Category_model.find({ category_name: args.category_name,delete:0 });
-        if (pre_name.length != 0) {
+        if (pre_name.length > 1) {
             return { ...args, ...{ info: { "msg": "This Category name was already selected", status: 'failed' } } };
         }
     }
@@ -268,10 +268,8 @@ module.exports.updateCategory = async (parent, args, { file }) => {
 module.exports.updatesubCategory = async (_, args, { file }) => {
     //console.log(args.file);
     if (args.subCategory_name) {
-        console.log("args==>",args.subCategory_name)
         var pre_name = await subCategory_model.find({ subCategory_name: args.subCategory_name,delete:0 });
-        console.log("prename",pre_name.length)
-        if (pre_name.length >= 1) {
+        if (pre_name.length >1) {
             return { ...args, ...{ info: { "msg": "This Sub Category name was already selected", status: 'failed' } } };
         }
     }
