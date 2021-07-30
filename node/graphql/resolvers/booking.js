76dab8d3d9848715834b9bf830a85bb26e0a3ea8
karthick <msk@waioz.com>
2021-07-30 11:30:59 +0530

    fixed price digit issues

--- node/graphql/resolvers/booking.js
+++ node/graphql/resolvers/booking.js
@@ -130,8 +130,8 @@ module.exports.get_payout = async (root, args) => {
     var data = await Booking_model.aggregate(filter);
     //console.log("Vishnu");
     //console.log(total);
-    console.log(datas[0]);
-    var pageInfo = { totalDocs: total.length, page: args.page, total_amount: datas[0] ? String(parseFloat(datas[0].total_amount).toFixed(2)) : 0.00 }
+    // console.log(datas[0]);
+    var pageInfo = { totalDocs: total.length, page: args.page, total_amount: datas[0] ? String(parseFloat(datas[0].total_amount).toFixed(2)) : "0.00" }
     return { pageInfo, data };
 }
 
