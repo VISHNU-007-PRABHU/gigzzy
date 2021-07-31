bbe5c51bf8bf1dbdbe07cc9c566035b2baff9c27
karthick <msk@waioz.com>
2021-07-31 17:38:39 +0530

    fix chil issues

--- node/graphql/resolvers/category.js
+++ node/graphql/resolvers/category.js
@@ -18,10 +18,11 @@ module.exports.category = async (parent, args, context, info) => {
     args.delete = 0;
     args.is_block = false;
     var result = await Category_model.find(args);
-    console.log(result);
-    _.map(result,data=>{
+    console.log(_.size(result),"ops");
+    result = _.map(result,data=>{
         if(_.size(result.child_category))return data
     })
+    console.log(_.size(result),"ops");
     return result;
 };
 
