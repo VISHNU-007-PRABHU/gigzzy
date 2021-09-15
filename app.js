904ccc510dbee463180e0103c70dfd5e0610f067
vishnu <vishnu@waioz.com>
2021-09-15 20:17:28 +0530

    add new db link

--- app.js
+++ app.js
@@ -318,7 +318,7 @@ app.use(cors({ origin: 'http://localhost:3000' }));
 
 
 
-mongoose.connect(process.env.DB_LINK).then(() => {
+mongoose.connect("mongodb://localhost/gigzzy").then(() => {
 }).catch((err) => {
   // console.log("Not Connected to Database ERROR! ", err);
 });
