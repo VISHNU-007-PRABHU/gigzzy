ac8cdb7a025dd4df7cd48eb157bc149cbd83749f
vishnu <vishnu@waioz.com>
2021-10-21 11:41:12 +0530

    add return status

--- node/graphql/resolvers/milestone.js
+++ node/graphql/resolvers/milestone.js
@@ -236,10 +236,9 @@ exports.manage_milestone_booking = async (root, args) => {
             let add_start_milestone = new BidingMilestone_model(update_detail)
             await add_start_milestone.save()
             await ContractJob_model.updateOne({ _id: args['contract_id'] }, { booking_status: commonHelper.bookink_status.ACCEPT }).exec()
-            let findBooking = {}
-            findBooking['msg'] = "contract milestone updated has been started";
-            findBooking['status'] = 'success';
-            return findBookin
+            add_start_milestone['msg'] = "contract milestone updated has been started";
+            add_start_milestone['status'] = 'success';
+            return add_start_milestone
         } else if (args.booking_status === 9) {
             await BidingMilestone_model.updateOne({ _id: args._id }, { booking_status: 14, end_date: moment.utc().format() }).exec()
             await ContractJob_model.updateOne({ _id: args['contract_id'] }, { booking_status: commonHelper.bookink_status.START }).exec()
