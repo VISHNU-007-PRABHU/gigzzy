08a4c95ab5b2986c5bcb65c13d6899a3760381ac
vishnu <vishnu@waioz.com>
2021-10-29 10:46:06 +0530

    add status

---
+++ src/component/Comman/booking_status.js
@@ -0,0 +1,29 @@
+export const BOOKING_STATUS = {
+    PROVIDER_ROLE: 2,
+    PROVIDER_ROLE: 2,
+    WAITING_ADMIN: 15,
+    WAITING_PAYMENT: 50,
+    ACCEPT: 9,
+    BOOKING: 12,
+    PENDING: 10,
+    START: 4,
+    ONGOING: 4,
+    CANCEL: 8,
+    END: 13,
+    COMPLETE: 14,
+};
+
+export const BOOKING_STATUS_REVERSE = {
+    2: 'PROVIDER ROLE',
+    2: "PROVIDER ROLE",
+    15: 'WAITING ADMIN',
+    50: 'WAITING PAYMENT',
+    9: 'ACCEPT',
+    12: 'BOOKING',
+    10: 'PENDING',
+    4: 'START',
+    4: 'ONGOING',
+    8: 'CANCEL',
+    13: 'END',
+    14: 'COMPLETE',
+};
